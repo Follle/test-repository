abd91569ba342dfb227faf87477b2bca639a499a
Angelina <folll.llle@gmail.com>
2024-11-28 15:43:45 +0300

    feat: Added new feature

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [greeting, setGreeting] = useState("Это тестовый проект Сулимовай Ангелины Алексеевны");
 
-  const greeting = "Это тестовый проект Сулимовай Ангелины Алексеевны";
+  const changeGreeting = () => {
+    setGreeting("Текст изменен!");
+  };
 
-  // console.log('This is App ');
   return (
     <div className="App">
-     <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-         {greeting}
+        <p>{greeting}</p>
+        <button onClick={changeGreeting}>Изменить текст</button>
       </header>
     </div>
   );
